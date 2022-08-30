d46084a4877480d04bf2cf7c9adf430dfb0831e3
Pinapple23 <92719911+Pinapple23@users.noreply.github.com>
2022-08-30 14:59:43 +0200

    weather app

---
+++ react-weather-app-main/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '056e757169mshcf9c98f4412b44ap122be9jsn5c1f1cb51cfc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "eb39e2d1195591db2ebf3f0c6e58a615"
